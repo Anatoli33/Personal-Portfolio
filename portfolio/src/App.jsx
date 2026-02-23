b7ee3377d4435e7a4753da1460e04f42ac857e92
Anatoli33 <hadjiev1215@gmail.com>
2026-02-23 15:26:36 +0200

    Add styling and new text.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 function App() {
   return (
     <>
+      <div className='headings'>
       <h1>Anatoli Hadzhiev</h1>
       <h2>Junior Front-end Delevoper</h2>
-      <p>Junior Front-End Developer focused on building fast, clean, and responsive web applications.</p>
+      </div>
+      <p>Junior Front-End Developer experienced in building responsive web applications with modern JavaScript (ES6+), React, and Next.js. Comfortable with Git, REST APIs, Chrome DevTools, and turning Figma designs into functional, user-friendly interfaces. Motivated to grow and contribute to real-world, client-focused projects in a team environment.</p>
     </>
   )
 }
