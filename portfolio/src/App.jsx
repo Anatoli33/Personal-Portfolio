f9dcdead020f86a4793d23641b4af4abbd948719
Anatoli33 <hadjiev1215@gmail.com>
2026-02-23 15:04:03 +0200

    HTML structure without CSS

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Start</h1>
+      <h1>Anatoli Hadzhiev</h1>
+      <h2>Junior Front-end Delevoper</h2>
+      <p>Junior Front-End Developer focused on building fast, clean, and responsive web applications.</p>
     </>
   )
 }
