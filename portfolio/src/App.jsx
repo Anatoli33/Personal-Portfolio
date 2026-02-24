aa92d9bd72b6f1a554dbd29d3c75291ca8ad43cf
Anatoli33 <hadjiev1215@gmail.com>
2026-02-24 10:40:35 +0200

    Add projects comp

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './comps/Home.jsx'
 import Navigation from './comps/Navigation';
+import Projects from './comps/Projects.jsx';
 
 function App() {
   return (
     <>
       <Navigation/>
       <Home/>
+      <Projects/>
     </>
   )
 }
---
+++ portfolio/src/comps/Projects.jsx
@@ -0,0 +1,18 @@
+// import "./projects.css";
+
+export default function Projects() {
+    return(
+        <>
+        <h1>Projects</h1>
+        <h2>Sport Talk</h2>
+        <p>Course Project</p>
+        <p>React web application with Firebase for authentication and real-time data. Allows users to create and interact with posts, comments, and polls through a clean, responsive interface.</p>
+        <h2>Movie Magic</h2>
+        <p>Coursework</p>
+        <p>A dynamic movie catalog website built with Node.js, Express, and Handlebars. Features include user authentication, interactive flip-card movie displays, searchable movie listings, and detailed movie pages.</p>
+        <h2>Main Quest</h2>        
+        <p>Personal Project</p>
+        <p> SPA built with JS, lit-html, and page.js to help students prepare for exams through interactive quizzes. Features include randomized questions, instant feedback, and a clean, responsive interface.</p>
+        </>
+    );
+}
