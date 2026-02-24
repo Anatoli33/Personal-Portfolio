c7573ffe2a14c4b6c76f47f1504b84d5195d15a1
Anatoli33 <hadjiev1215@gmail.com>
2026-02-24 10:22:04 +0200

    Add home as a comp

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './comps/Home.jsx'
 
 function App() {
   return (
@@ -13,15 +14,8 @@ function App() {
           <li>Contact me</li>
         </ul>
       </nav>
-    </header>
-      <div className='body'>
-      <div className='text'>
-      <h1>Anatoli Hadzhiev</h1>
-      <h2>Junior Front-end Delevoper</h2>
-      <p>Junior Front-End Developer experienced in building responsive web applications with modern JavaScript (ES6+), React, and Next.js. Comfortable with Git, REST APIs, Chrome DevTools, and turning Figma designs into functional, user-friendly interfaces. Motivated to grow and contribute to real-world, client-focused projects in a team environment.</p>
-      </div>
-      <img src="./prof-pic.png" alt="Profile-Pic" />
-      </div>
+      </header>
+      <Home/>
     </>
   )
 }
---
+++ portfolio/src/comps/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return(
+      <div className='body'>
+      <div className='text'>
+      <h1>Anatoli Hadzhiev</h1>
+      <h2>Junior Front-end Delevoper</h2>
+      <p>Junior Front-End Developer experienced in building responsive web applications with modern JavaScript (ES6+), React, and Next.js. Comfortable with Git, REST APIs, Chrome DevTools, and turning Figma designs into functional, user-friendly interfaces. Motivated to grow and contribute to real-world, client-focused projects in a team environment.</p>
+      </div>
+      <img src="./prof-pic.png" alt="Profile-Pic" />
+      </div>
+    );
+}
