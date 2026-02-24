13875ed99be73c872a4a292aef8ccfce20cabad2
Anatoli33 <hadjiev1215@gmail.com>
2026-02-24 10:13:02 +0200

    Update the structure and CSS

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='headings'>
+      <div className='body'>
+      <div className='text'>
       <h1>Anatoli Hadzhiev</h1>
       <h2>Junior Front-end Delevoper</h2>
-      </div>
       <p>Junior Front-End Developer experienced in building responsive web applications with modern JavaScript (ES6+), React, and Next.js. Comfortable with Git, REST APIs, Chrome DevTools, and turning Figma designs into functional, user-friendly interfaces. Motivated to grow and contribute to real-world, client-focused projects in a team environment.</p>
+      </div>
+      <img src="./prof-pic.png" alt="Profile-Pic" />
+      </div>
     </>
   )
 }
