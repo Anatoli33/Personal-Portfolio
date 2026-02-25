01f33e3866c0dcb777da5729db544590d9b96200
Anatoli33 <hadjiev1215@gmail.com>
2026-02-25 11:51:28 +0200

    Some CSS for the contact form

--- portfolio/src/comps/Contact.jsx
+++ portfolio/src/comps/Contact.jsx
@@ -1,28 +1,34 @@
-// import "./contact.css";
-
+import "./contact.css";
 export default function Contact() {
-    return(
-        <section id="contact">
+  return (
+    <section id="contact">
       <h2>Contact Me</h2>
       <p>If you have any questions or want to work together, feel free to send me a message.</p>
       <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <br />
-        <label>
-          Message:
-          <textarea name="message"></textarea>
-        </label>
-        <br />
-        <button type="submit">Send Message</button>
+        <fieldset>
+          <legend>Your Information</legend>
+
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <br />
+
+          <label>
+            Email:
+            <input type="email" name="email" />
+          </label>
+          <br />
+
+          <label>
+            Message:
+            <textarea name="message"></textarea>
+          </label>
+          <br />
+
+          <button type="submit">Send Message</button>
+        </fieldset>
       </form>
     </section>
-    );
+  );
 }
