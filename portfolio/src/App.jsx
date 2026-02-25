40ac618dfda9648746a6ff0277d9ce03c20a3c99
Anatoli33 <hadjiev1215@gmail.com>
2026-02-25 12:00:08 +0200

    SImple home section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,11 +3,13 @@ import Contact from './comps/Contact.jsx';
 import Navigation from './comps/Navigation';
 import Projects from './comps/Projects.jsx';
 import About from './comps/About';
+import Home from './comps/Home.jsx';
 
 function App() {
   return (
     <>
       <Navigation/>
+      <Home/>
       <About/>
       <Projects/>
       <Contact/>
--- portfolio/src/comps/Home.jsx
+++ portfolio/src/comps/Home.jsx
@@ -1,7 +1,13 @@
-// import "./home.css";
 
-// export default function Home() {
-//     return(
 
-//     );
-// }
+export default function Home() {
+  return (
+    <section id="home">
+      <h1>Hello, I'm Anatoli Hadzhiev</h1>
+      <p>I'm a junior front-end developer building clean and responsive web apps.</p>
+      <a href="#projects">
+        <button>View My Projects</button>
+      </a>
+    </section>
+  );
+}
