15778f42218e6f71d24994db1861e11c3eb03ba4
Anatoli33 <hadjiev1215@gmail.com>
2026-03-03 15:03:48 +0200

    Add new project

--- portfolio/src/comps/Projects.jsx
+++ portfolio/src/comps/Projects.jsx
@@ -13,6 +13,10 @@ export default function Projects() {
         <h2>Main Quest</h2>        
         <p>Personal Project</p>
         <p className="text"> SPA built with JS, lit-html, and page.js to help students prepare for exams through interactive quizzes. Features include randomized questions, instant feedback, and a clean, responsive interface.</p>
+        <h2>Mini Tech Shop</h2>        
+        <p>Personal Project</p>
+        <p className="text"> Modern React e-commerce demo with product slider, responsive grid, and fully functional shopping cart. Built with React Context for global state management, React Router for dynamic product pages, and Swiper.js for interactive UI. Focused on clean architecture, reusable components, and real-world front-end patterns.</p>
+   
         </div>
     );
 }
