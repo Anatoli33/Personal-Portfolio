02cf583f7c249c3fa37b735d146a8a1cd9b56961
Anatoli33 <hadjiev1215@gmail.com>
2026-03-03 14:43:05 +0200

    Contact form JSX update

--- portfolio/src/comps/Contact.jsx
+++ portfolio/src/comps/Contact.jsx
@@ -1,34 +1,41 @@
 import "./contact.css";
+
 export default function Contact() {
   return (
-    <section id="contact">
-      <h2>Contact Me</h2>
-      <p>If you have any questions or want to work together, feel free to send me a message.</p>
-      <form>
-        <fieldset>
-          <legend>Your Information</legend>
+    <section className="contact">
+      <div className="contact-container">
+        <h2>Contact Me</h2>
+        <p>
+          If you have a project in mind or want to collaborate, feel free to
+          send me a message. I'll get back to you as soon as possible.
+        </p>
 
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <br />
+        <form className="contact-form">
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input id="name" type="text" name="name" required />
+          </div>
 
-          <label>
-            Email:
-            <input type="email" name="email" />
-          </label>
-          <br />
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" name="email" required />
+          </div>
 
-          <label>
-            Message:
-            <textarea name="message"></textarea>
-          </label>
-          <br />
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              rows="5"
+              required
+            ></textarea>
+          </div>
 
-          <button type="submit">Send Message</button>
-        </fieldset>
-      </form>
+          <button type="submit" className="btn primary">
+            Send Message
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
